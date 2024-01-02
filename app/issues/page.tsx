2cc5e1be068f2540457130dc0b8ee5151bf56f37
Hasib Ahmed <hasibahmed1054@gmail.com>
2024-01-02 19:12:21 +0600

    show issue details

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -31,7 +31,8 @@ const  IssuesPage = async () => {
           {issues.map((issue) => (
             <TableRow key={issue.id}>
               <TableCell>
-                {issue.title}
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+
                 <div className="block md:hidden">
                   <IssueStatusBadge status={issue.status} />
                 </div>
